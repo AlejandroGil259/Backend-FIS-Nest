fdd6c3720bd978132909d21ab19ae5e0d1e50109
AlejandroGil259 <sergio.gile@usantoto.edu.co>
2023-05-26 20:06:43 -0500

    Agregar dto, service, controller

--- src/proyectos/dto/create-proyecto.dto.ts
+++ src/proyectos/dto/create-proyecto.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProyectoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, IsEnum } from 'class-validator';
+import { OPCION_GRADO } from '../constants';
+
+export class CreateProyectoDto {
+  @ApiProperty({ description: 'Referencia del proyecto' })
+  @IsOptional()
+  @IsString()
+  idReferencia: string;
+
+  @ApiProperty({ enum: OPCION_GRADO })
+  @IsEnum(OPCION_GRADO)
+  opcionGrado: OPCION_GRADO;
+
+  @ApiProperty({ description: 'Titulo del proyecto' })
+  @IsString()
+  titulo: string;
+}
--- src/proyectos/entities/proyecto.entity.ts
+++ src/proyectos/entities/proyecto.entity.ts
@@ -3,37 +3,37 @@ import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../commons/entities/base-entity.entity';
 import { OPCION_GRADO } from '../constants';
 
-@Entity( 'proyectos' )
+@Entity('proyectos')
 export class Proyecto extends BaseEntity {
-    @ApiProperty( { uniqueItems: true } )
-    @Column( {
-        primary: true,
-        type: 'uuid',
-    } )
-    idProyecto: string;
+  @ApiProperty({ uniqueItems: true })
+  @Column({
+    primary: true,
+    type: 'uuid',
+  })
+  idProyecto: string;
 
-    @ApiProperty()
-    @Column()
-    idReferencia: string;
+  @ApiProperty()
+  @Column()
+  idReferencia: string;
 
-    @ApiProperty( { enum: OPCION_GRADO } )
-    @Column( {
-        type: 'varchar',
-        enum: OPCION_GRADO
-    } )
-    opcionGrado: OPCION_GRADO;
+  @ApiProperty({ enum: OPCION_GRADO })
+  @Column({
+    type: 'varchar',
+    enum: OPCION_GRADO,
+  })
+  opcionGrado: OPCION_GRADO;
 
-    @ApiProperty( {
-        description: 'Estado del proyecto en la plataforma',
-        default: true,
-    } )
-    @Column( {
-        type: 'bool',
-        default: true,
-    } )
-    estado: boolean;
+  @ApiProperty({
+    description: 'Estado del proyecto en la plataforma',
+    default: true,
+  })
+  @Column({
+    type: 'bool',
+    default: true,
+  })
+  estado: boolean;
 
-    @ApiProperty( { description: 'Titulo del proyecto' } )
-    @Column()
-    titulo: string;
+  @ApiProperty({ description: 'Titulo del proyecto' })
+  @Column()
+  titulo: string;
 }
