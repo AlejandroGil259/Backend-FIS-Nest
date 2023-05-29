181e557106e9797647905a534882c8b404272f87
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-05-29 18:41:41 -0500

    Add deactivate/restore endpoints

--- src/usuarios/services/usuarios.service.ts
+++ src/usuarios/services/usuarios.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
@@ -9,87 +9,106 @@ import { Usuario } from '../entities/usuarios.entity';
 
 @Injectable()
 export class UsuariosService {
-  constructor(
-    @InjectRepository(Usuario)
-    private readonly usuarioRepo: Repository<Usuario>,
-  ) {}
+    constructor (
+        @InjectRepository( Usuario )
+        private readonly usuarioRepo: Repository<Usuario>,
+    ) { }
 
-  async register(createUserDto: CreateUserDto) {
-    try {
-      const { contrasena, ...usuario } = createUserDto;
+    async register ( createUserDto: CreateUserDto ) {
+        try {
+            const { contrasena, ...usuario } = createUserDto;
 
-      const user = this.usuarioRepo.create({
-        ...usuario,
-        contrasena: bcrypt.hashSync(contrasena, 10),
-      });
+            const user = this.usuarioRepo.create( {
+                ...usuario,
+                contrasena: bcrypt.hashSync( contrasena, 10 ),
+            } );
 
-      await this.usuarioRepo.save(user);
+            await this.usuarioRepo.save( user );
 
-      delete user.contrasena;
+            delete user.contrasena;
 
-      // TODO: Crear JWT con información de autorización
+            // TODO: Crear JWT con información de autorización
 
-      return {
-        usuario: user,
-      };
-    } catch (error) {
-      throw DBExceptionService.handleDBException(error);
-    }
-  }
-
-  async findAll() {
-    const usuarios = await this.usuarioRepo.find();
-
-    if (!usuarios || !usuarios.length)
-      throw new NotFoundException('No se encontraron resultados');
-
-    return usuarios;
-  }
-
-  async findOne(documento: number) {
-    const usuario = await this.usuarioRepo.findOneBy({ documento });
-
-    if (!usuario)
-      throw new NotFoundException(
-        `No se encontraron resultados para el documento "${documento}"`,
-      );
-
-    return usuario;
-  }
-
-  async update(documento: number, updateUsuarioDto: UpdateUsuarioDto) {
-    // const usuario = await this.usuarioRepo.findOne({
-    //   where: { documento },
-    // });
-
-    // if (!usuario) {
-    //   throw new NotFoundException('Este usuario no existe');
-    // }
-
-    // const actualizarUsuario = {
-    //   ...usuario,
-    //   ...updateUsuarioDto,
-    // };
-
-    // return await this.usuarioRepo.save(actualizarUsuario);
-
-    const usuario = await this.usuarioRepo.findOne({ where: { documento } });
-
-    if (!usuario) throw new NotFoundException('Este usuario no existe');
-    const actualizarUsuario = Object.assign(usuario, updateUsuarioDto);
-
-    return await this.usuarioRepo.save(actualizarUsuario);
-  }
-
-  async remove(documento: number) {
-    const usuario = await this.usuarioRepo.findOne({
-      where: { documento },
-    });
-
-    if (!usuario) {
-      throw new NotFoundException('Este usuario no existe');
+            return {
+                usuario: user,
+            };
+        } catch ( error ) {
+            throw DBExceptionService.handleDBException( error );
+        }
     }
 
-    await this.usuarioRepo.remove(usuario);
-  }
+    async findAll () {
+        const usuarios = await this.usuarioRepo.find();
+
+        if ( !usuarios || !usuarios.length )
+            throw new NotFoundException( 'No se encontraron resultados' );
+
+        return usuarios;
+    }
+
+    async findOne ( documento: number ) {
+        const usuario = await this.usuarioRepo.findOneBy( { documento } );
+
+        if ( !usuario )
+            throw new NotFoundException( `No se encontraron resultados para el documento "${ documento }"` );
+
+        return usuario;
+    }
+
+    async update ( documento: number, updateUsuarioDto: UpdateUsuarioDto ) {
+        try {
+            const usuario = await this.usuarioRepo.findOneBy( { documento } );
+            if ( !usuario ) return new NotFoundException( `No se encontró un usuario con el documento ${ documento }` );
+
+            return await this.usuarioRepo.update( { documento }, { ...updateUsuarioDto } );
+        } catch ( error ) {
+            throw DBExceptionService.handleDBException( error );
+        }
+    }
+
+    async deactivate ( documento: number ) {
+        const usuario = await this.usuarioRepo.findOne( { where: { documento }, withDeleted: false } );
+
+        if ( !usuario )
+            throw new NotFoundException( `El usuario con el documento ${ documento }, ya se encuentra eliminado o no existe en la base de datos` );
+
+        const { affected } = await this.usuarioRepo.update( { documento }, {
+            estado: false,
+            deletedAt: new Date()
+        } );
+
+        if ( !affected || affected === 0 )
+            throw new BadRequestException( `No se pudo eliminar el usuario con el documento ${ documento }` );
+
+        return 'El usuario fue eliminado/desactivado correctamente';
+    }
+
+    async restore ( documento: number ) {
+        const usuario = await this.usuarioRepo.findOne( { where: { documento }, withDeleted: true } );
+
+        if ( !usuario )
+            throw new NotFoundException( `No se encontró un usuario con el documento ${ documento } ` );
+
+        if ( usuario.estado )
+            throw new BadRequestException( `El usuario con el documento ${ documento } ya se encuentra activo` );
+
+        const { affected } = await this.usuarioRepo.update( { documento }, {
+            estado: true,
+            deletedAt: null
+        } );
+
+        if ( !affected || affected === 0 )
+            throw new BadRequestException( `No se pudo activar el usuario con el documento ${ documento }` );
+
+        return 'El usuario fue restaurado/activado correctamente';
+    }
+
+    async remove ( documento: number ) {
+        const usuario = await this.usuarioRepo.findOne( { where: { documento }, withDeleted: true } );
+
+        if ( !usuario )
+            throw new NotFoundException( `No se encontró un usuario con el documento ${ documento } ` );
+
+        return await this.usuarioRepo.remove( usuario );
+    }
 }
