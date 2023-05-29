e9e4069a89aa8982734cbf223bb4055c913e9794
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-05-29 18:41:17 -0500

    Delete patch/remove endpoints

--- src/notificaciones/notificaciones.service.ts
+++ src/notificaciones/notificaciones.service.ts
@@ -3,73 +3,45 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { DBExceptionService } from 'src/commons/services/db-exception.service';
 import { Repository } from 'typeorm';
 import { CreateNotificacionesDto } from './dto/create-notificaciones.dto';
-import { UpdateNotificacioneDto } from './dto/update-notificaciones.dto';
 import { Notificaciones } from './entities/notificaciones.entity';
 
 @Injectable()
 export class NotificacionesService {
-  constructor(
-    @InjectRepository(Notificaciones)
-    private readonly notificacionRepo: Repository<Notificaciones>,
-  ) {}
+    constructor (
+        @InjectRepository( Notificaciones )
+        private readonly notificacionRepo: Repository<Notificaciones>,
+    ) { }
 
-  async create(createNotificacionDto: CreateNotificacionesDto) {
-    try {
-      const notification = await this.notificacionRepo.save(
-        createNotificacionDto,
-      );
-      return {
-        notificacion: notification,
-      };
-    } catch (error) {
-      throw DBExceptionService.handleDBException(error);
-    }
-  }
-
-  async findAll() {
-    const notificaciones = await this.notificacionRepo.find();
-
-    if (!notificaciones || !notificaciones.length)
-      throw new NotFoundException('No se encontraron resultados');
-
-    return notificaciones;
-  }
-
-  async findOne(id: string) {
-    const notificacion = await this.notificacionRepo.findOneBy({ id });
-
-    if (!notificacion)
-      throw new NotFoundException(
-        `No se encontraron resultados para notificacion "${id}"`,
-      );
-
-    return notificacion;
-  }
-
-  async update(id: string, updateNotificacioneDto: UpdateNotificacioneDto) {
-    const notificacion = await this.notificacionRepo.findOne({
-      where: { id },
-    });
-
-    if (!notificacion)
-      throw new NotFoundException('Esta notificación no existe');
-    const actualizarNotificacion = Object.assign(
-      notificacion,
-      updateNotificacioneDto,
-    );
-
-    return await this.notificacionRepo.save(actualizarNotificacion);
-  }
-
-  async remove(id: string) {
-    const notificacion = await this.notificacionRepo.findOne({
-      where: { id },
-    });
-
-    if (!notificacion) {
-      throw new NotFoundException('Esta notificación no existe');
+    async create ( createNotificacionDto: CreateNotificacionesDto ) {
+        try {
+            const notification = await this.notificacionRepo.save(
+                createNotificacionDto,
+            );
+            return {
+                notificacion: notification,
+            };
+        } catch ( error ) {
+            throw DBExceptionService.handleDBException( error );
+        }
     }
 
-    await this.notificacionRepo.remove(notificacion);
-  }
+    async findAll () {
+        const notificaciones = await this.notificacionRepo.find();
+
+        if ( !notificaciones || !notificaciones.length )
+            throw new NotFoundException( 'No se encontraron resultados' );
+
+        return notificaciones;
+    }
+
+    async findOne ( id: string ) {
+        const notificacion = await this.notificacionRepo.findOneBy( { id } );
+
+        if ( !notificacion )
+            throw new NotFoundException(
+                `No se encontraron resultados para notificacion "${ id }"`,
+            );
+
+        return notificacion;
+    }
 }
--- src/novedades/novedades.service.ts
+++ src/novedades/novedades.service.ts
@@ -8,59 +8,47 @@ import { Novedades } from './entities/novedades.entity';
 
 @Injectable()
 export class NovedadesService {
-  constructor(
-    @InjectRepository(Novedades)
-    private readonly novedadRepo: Repository<Novedades>,
-  ) {}
-  async create(createNovedadDto: CreateNovedadesDto) {
-    try {
-      const novelty = await this.novedadRepo.save(createNovedadDto);
-      return {
-        novedad: novelty,
-      };
-    } catch (error) {
-      throw DBExceptionService.handleDBException(error);
-    }
-  }
-
-  async findAll() {
-    const novedades = await this.novedadRepo.find();
-
-    if (!novedades || novedades.length)
-      throw new NotFoundException('No se encontraron resultados');
-
-    return novedades;
-  }
-
-  async findOne(id: string) {
-    const novedad = await this.novedadRepo.findOneBy({ id });
-
-    if (!novedad)
-      throw new NotFoundException(
-        `No se encontraron resultados para la novedad "${id}"`,
-      );
-
-    return novedad;
-  }
-
-  async update(id: string, updateNovedadeDto: UpdateNovedadeDto) {
-    const novedad = await this.novedadRepo.findOne({ where: { id } });
-
-    if (!novedad) throw new NotFoundException('Esta novedad no existe');
-    const actualizarNovedad = Object.assign(novedad, updateNovedadeDto);
-
-    return await this.novedadRepo.save(actualizarNovedad);
-  }
-
-  async remove(id: string) {
-    const novedad = await this.novedadRepo.findOne({
-      where: { id },
-    });
-
-    if (!novedad) {
-      throw new NotFoundException('Este usuario no existe');
+    constructor (
+        @InjectRepository( Novedades )
+        private readonly novedadRepo: Repository<Novedades>,
+    ) { }
+    async create ( createNovedadDto: CreateNovedadesDto ) {
+        try {
+            const novelty = await this.novedadRepo.save( createNovedadDto );
+            return {
+                novedad: novelty,
+            };
+        } catch ( error ) {
+            throw DBExceptionService.handleDBException( error );
+        }
     }
 
-    await this.novedadRepo.remove(novedad);
-  }
+    async findAll () {
+        const novedades = await this.novedadRepo.find();
+
+        if ( !novedades || novedades.length )
+            throw new NotFoundException( 'No se encontraron resultados' );
+
+        return novedades;
+    }
+
+    async findOne ( id: string ) {
+        const novedad = await this.novedadRepo.findOneBy( { id } );
+
+        if ( !novedad )
+            throw new NotFoundException(
+                `No se encontraron resultados para la novedad "${ id }"`,
+            );
+
+        return novedad;
+    }
+
+    async update ( id: string, updateNovedadeDto: UpdateNovedadeDto ) {
+        const novedad = await this.novedadRepo.findOne( { where: { id } } );
+
+        if ( !novedad ) throw new NotFoundException( 'Esta novedad no existe' );
+        const actualizarNovedad = Object.assign( novedad, updateNovedadeDto );
+
+        return await this.novedadRepo.save( actualizarNovedad );
+    }
 }
