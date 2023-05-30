6192ab4f6b00d1b7af31cfe145c6232858559034
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-05-30 18:44:11 -0500

    Fix Entities

--- src/notificaciones/entities/notificacion.entity.ts
+++ src/notificaciones/entities/notificacion.entity.ts
@@ -10,53 +10,53 @@ import {
 import { BaseEntity } from '../../commons/entities/base-entity.entity';
 import { TIPO_NOTIFICACION } from '../constansts';
 import { Novedad } from '../../novedades/entities/novedad.entity';
-import { UsuariosNotificaciones } from '../../usuarios/entities/usuarios.entity';
+import { UsuariosNotificaciones } from '../../usuarios/entities/usuarios-notificaciones.entity';
 import { Proyecto } from '../../proyectos/entities/proyecto.entity';
 
-@Entity('notificaciones')
+@Entity( 'notificaciones' )
 export class Notificacion extends BaseEntity {
-  @ApiProperty({
+  @ApiProperty( {
     uniqueItems: true,
     example: '4b87d547-ddc1-4e80-acdf-f1cd722f9f5',
-  })
-  @PrimaryGeneratedColumn('uuid')
+  } )
+  @PrimaryGeneratedColumn( 'uuid' )
   id: string;
 
-  @ApiProperty({ example: new Date().toISOString() })
+  @ApiProperty( { example: new Date().toISOString() } )
   @Column()
   fechaNotificacion: Date;
 
-  @ApiProperty({ example: '123456789' })
-  @Column({ type: 'int8' })
+  @ApiProperty( { example: '123456789' } )
+  @Column( { type: 'int8' } )
   usuarioEnvio: number;
 
-  @ApiProperty({ example: 'Titulo de la notificación' })
+  @ApiProperty( { example: 'Titulo de la notificación' } )
   @Column()
   titulo: string;
 
-  @ApiProperty({ example: 'descripcion' })
+  @ApiProperty( { example: 'descripcion' } )
   @Column()
   descripcion: string;
 
-  @ApiProperty({
+  @ApiProperty( {
     enum: TIPO_NOTIFICACION,
-  })
-  @Column({
+  } )
+  @Column( {
     type: 'varchar',
     enum: TIPO_NOTIFICACION,
-  })
+  } )
   tipoNotificacion: TIPO_NOTIFICACION;
 
-  @OneToOne(() => Novedad)
-  @JoinColumn({ name: 'id_novedad' })
+  @OneToOne( () => Novedad )
+  @JoinColumn( { name: 'id_novedad' } )
   novedad: Novedad;
 
-  @OneToMany(() => Proyecto, (notificacion) => notificacion.notificaciones)
+  @OneToMany( () => Proyecto, ( notificacion ) => notificacion.notificaciones )
   proyectos: Proyecto;
 
   @OneToMany(
     () => UsuariosNotificaciones,
-    (usuariosNotificacion) => usuariosNotificacion.notificaciones_id,
+    ( usuariosNotificacion ) => usuariosNotificacion.notificaciones_id,
   )
   usuariosNotificaciones: UsuariosNotificaciones[];
 }
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,21 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsuariosService } from './services/usuarios.service';
-import { UsuariosController } from './controllers/usuarios.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Usuario, UsuariosNotificaciones } from './entities/usuarios.entity';
-import { UsuariosProyectos } from './entities/usuarios-proyectos.entity';
 
-@Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forFeature([
-      Usuario,
-      UsuariosNotificaciones,
-      UsuariosProyectos,
-    ]),
-  ],
-  providers: [UsuariosService],
-  controllers: [UsuariosController],
-})
-export class UsuariosModule {}
+import { UsuariosController } from './controllers/usuarios.controller';
+import { UsuariosNotificaciones } from './entities/usuarios-notificaciones.entity';
+import { UsuariosProyectos } from './entities/usuarios-proyectos.entity';
+import { Usuario } from './entities/usuarios.entity';
+import { UsuariosService } from './services/usuarios.service';
+
+@Module( {
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forFeature( [
+            Usuario,
+            UsuariosNotificaciones,
+            UsuariosProyectos,
+        ] ),
+    ],
+    providers: [ UsuariosService ],
+    controllers: [ UsuariosController ],
+} )
+export class UsuariosModule { }
