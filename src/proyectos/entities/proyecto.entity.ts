558a7f47d2819e9566e92ca9eb729eefb8bce61a
AlejandroGil259 <sergio.gile@usantoto.edu.co>
2023-05-29 21:04:23 -0500

    add usuarios-proyectos

--- src/proyectos/entities/proyecto.entity.ts
+++ src/proyectos/entities/proyecto.entity.ts
@@ -4,6 +4,7 @@ import { BaseEntity } from '../../commons/entities/base-entity.entity';
 import { OPCION_GRADO } from '../constants';
 import { Archivo } from '../../archivos/entities/archivo.entity';
 import { Novedad } from '../../novedades/entities/novedad.entity';
+import { UsuariosProyectos } from '../../usuarios/entities/usuarios-proyectos.entity';
 
 @Entity('proyectos')
 export class Proyecto extends BaseEntity {
@@ -44,4 +45,10 @@ export class Proyecto extends BaseEntity {
 
   @OneToMany(() => Novedad, (novedad) => novedad.proyecto)
   novedades: Novedad[];
+
+  @OneToMany(
+    () => UsuariosProyectos,
+    (usuarioProyecto) => usuarioProyecto.usuarioDocumento,
+  )
+  usuariosProyectos: UsuariosProyectos[];
 }
