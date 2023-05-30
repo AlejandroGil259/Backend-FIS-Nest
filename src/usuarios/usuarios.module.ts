a6c92b684caa26f8885b9427b65eda279ef87e55
AlejandroGil259 <sergio.gile@usantoto.edu.co>
2023-05-29 21:05:05 -0500

    add entity usuarios-proyectos

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -3,10 +3,18 @@ import { UsuariosService } from './services/usuarios.service';
 import { UsuariosController } from './controllers/usuarios.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Usuario } from './entities/usuarios.entity';
+import { Usuario, UsuariosNotificaciones } from './entities/usuarios.entity';
+import { UsuariosProyectos } from './entities/usuarios-proyectos.entity';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([Usuario])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([
+      Usuario,
+      UsuariosNotificaciones,
+      UsuariosProyectos,
+    ]),
+  ],
   providers: [UsuariosService],
   controllers: [UsuariosController],
 })
