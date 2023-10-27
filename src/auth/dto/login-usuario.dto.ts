2d0390862865fde04f606c539c9907e7dc6ec804
AlejandroGil259 <sergio.gile@usantoto.edu.co>
2023-10-27 17:48:54 -0500

    update auth/contrasena

--- src/auth/dto/login-usuario.dto.ts
+++ src/auth/dto/login-usuario.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class LoginUsuarioDto {
   @ApiProperty({ example: 'example@usantoto.edu.co' })
@@ -16,7 +10,6 @@ export class LoginUsuarioDto {
   @ApiProperty({ example: 'Max-10' })
   @IsString()
   @MinLength(6)
-  @MaxLength(10)
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
       'La contraseña debe tener mínimo una letra mayúscula, una minúscula y un número',
